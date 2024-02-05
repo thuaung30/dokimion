b11e05765a56d02f910aa44038f949c38bab46fd
thuaung30 <thuhtetaung.d2@gmail.com>
2024-02-05 14:30:27 +0630

    feat: v2

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (_req, res) => {
   res.send("Dokimion")
 })
 
+app.get("/v2", (_req, res) => {
+  res.send("Dokimion v2")
+})
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
